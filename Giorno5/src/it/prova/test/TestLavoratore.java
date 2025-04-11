e7e6339d89be79de22a5322ca9720d2ebc814bf9
clash8 <g.lobianco97@gmail.com>
2025-04-11 11:05:51 +0200

    Giorno5 pt 4

--- Giorno5/src/it/prova/test/TestLavoratore.java
+++ Giorno5/src/it/prova/test/TestLavoratore.java
@@ -3,10 +3,14 @@ import it.prova.model.*;
 
 public class TestLavoratore {
     public static void main(String[] args) {
-        Lavoratore l1 = new Operaio("Mario", "Rossi", 1500);
-        Lavoratore l2 = new Volontario("Giovanni", "Verdi", "Associazione Volontari");
 
-        System.out.println(l1.percepisco());
-        System.out.println(l2.percepisco());
+        Lavoratore totti = new Operaio("Enrico", "Brignano", 1500);
+        System.out.println(totti.getNome() + " " + totti.getCognome() + ": " + totti.percepisco());
+
+        Lavoratore fiorello = new Volontario("Rosario", "Fiorello", "Croce Rossa");
+        System.out.println(fiorello.getNome() + " " + fiorello.getCognome() + ": " + fiorello.percepisco());
+
+        Lavoratore boldi = new Lavoratore("Massimo", "Boldi");
+        System.out.println(boldi.getNome() + " " + boldi.getCognome() + ": " + boldi.percepisco());
     }
 }

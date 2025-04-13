679e4a58f7a23d79b91e412dd4b843b514e0d355
Clash8 <g.lobianco97@gmail.com>
2025-04-13 17:09:39 +0200

    bub fixed

--- Giorno5/src/it/esercizipercasa/model/Biblioteca.java
+++ Giorno5/src/it/esercizipercasa/model/Biblioteca.java
@@ -16,69 +16,55 @@ public class Biblioteca {
 
     public void aggiungiMateriale( MaterialeBiblioteca m) {
         MaterialeBiblioteca[] nuoviMateriali = new MaterialeBiblioteca[materiali.length + 1];
-        for (int i = 0; i < materiali.length; i++) {
+        for (int i = 0; i < materiali.length; i++)
             nuoviMateriali[i] = materiali[i];
-        }
         nuoviMateriali[materiali.length] = m;
         materiali = nuoviMateriali;
 
     }
     public Biblioteca cercaMaterialePerTitolo(String titolo) {
         int count = 0;
-        for (MaterialeBiblioteca m : materiali) {
-            if (m.getTitolo().equalsIgnoreCase(titolo)) {
+        for (MaterialeBiblioteca m : materiali)
+            if (m.getTitolo().equalsIgnoreCase(titolo))
                 count++;
-            }
-        }
         MaterialeBiblioteca[] risultato = new MaterialeBiblioteca[count];
         count = 0;
-        for (MaterialeBiblioteca m : materiali) {
-            if (m.getTitolo().equalsIgnoreCase(titolo)) {
+        for (MaterialeBiblioteca m : materiali)
+            if (m.getTitolo().equalsIgnoreCase(titolo))
                 risultato[count++] = m;
-            }
-        }
         return new Biblioteca(risultato);
     }
 
     public Biblioteca elencaMaterialiDisponibili() {
         int count = 0;
-        for (MaterialeBiblioteca m : materiali) {
-            if (m.isDisponibile()) {
+        for (MaterialeBiblioteca m : materiali)
+            if (m.isDisponibile())
                 count++;
-            }
-        }
         MaterialeBiblioteca[] risultato = new MaterialeBiblioteca[count];
         count = 0;
-        for (MaterialeBiblioteca m : materiali) {
-            if (m.isDisponibile()) {
+        for (MaterialeBiblioteca m : materiali)
+            if (m.isDisponibile())
                 risultato[count++] = m;
-            }
-        }
         return new Biblioteca(risultato);
     }
     public Biblioteca elencaMaterialiInPrestito() {
         int count = 0;
-        for (MaterialeBiblioteca m : materiali) {
-            if (!m.isDisponibile()) {
+        for (MaterialeBiblioteca m : materiali)
+            if (!m.isDisponibile())
                 count++;
-            }
-        }
         MaterialeBiblioteca[] risultato = new MaterialeBiblioteca[count];
         count = 0;
-        for (MaterialeBiblioteca m : materiali) {
-            if (!m.isDisponibile()) {
+        for (MaterialeBiblioteca m : materiali)
+            if (!m.isDisponibile())
                 risultato[count++] = m;
-            }
-        }
         return new Biblioteca(risultato);
     }
 
     @Override
     public String toString() {
         String result = "Biblioteca con " + materiali.length + " materiali:\n";
-        for (MaterialeBiblioteca m : materiali) {
+        for (MaterialeBiblioteca m : materiali)
             result += "\t" + m.toString();
-        }
         return result;
     }
 }
